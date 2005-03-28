3c3f9e06880972a59190e1ab1aaf87e9a94b4766
Guillaume Outters <guillaume-pasTeX@outters.eu>
2005-03-28 23:01:28 +0200

    oubli du titre dans la lecture
    = Comme promis dans un précédent paquet, ceci n'est qu'une amélioration. Le mot « régression » ressemble trop à « agression ».
    
    darcs-hash:ec50ebcc7846a1764784d223cbcd44fdb446bd54

--- compo/de_xml.php
+++ compo/de_xml.php
@@ -122,7 +122,7 @@ class De_Xml extends CompoAProprietes
 {
 	function De_Xml()
 	{
-		$this->CompoAProprietes(array('perso' => 1, 'formation' => 1, 'expérience' => 1, 'langues' => 1, 'connaissances' => 1, 'intérêts' => 1, 'loisirs' => 1), array());
+		$this->CompoAProprietes(array('perso' => 1, 'titre' => 0, 'formation' => 1, 'expérience' => 1, 'langues' => 1, 'connaissances' => 1, 'intérêts' => 1, 'loisirs' => 1), array());
 		$this->chargeur = new Chargeur();
 	}
 	
