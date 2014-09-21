5ef71ed3b392435ff61fe1455cc9e341d226fcfc
Guillaume Outters <guillaume-pasTeX@outters.eu>
2014-09-21 22:34:25 +0200

    html2: jointurePotDEchap
    + Jointure en forme de pot d'échappement.
    
    darcs-hash:0e1538f10ffcd467e6d97dea71162e3cbf648f54

--- decompo/html2/html2.js
+++ decompo/html2/html2.js
@@ -45,6 +45,50 @@ LignesTemps.jointureSimple = function(pTexte, pBloc)
 	return d;
 };
 
+LignesTemps.jointurePotDEchap = function(pTexte, pBloc)
+{
+	var embouchureB = 10
+	var courbeB = 30;
+	var courbeT = 30;
+	var embouchureT = 30;
+	var demieLargeurTube = 5;
+	
+	var d = '';
+	// Le tube central est composé de deux lignes parallèles à a * x + y = b (chacune avec un offset de n pixels par rapport à cette ligne centrale).
+	var ptB = { x: pBloc.x + embouchureB, y: pBloc.ym };
+	var ptT = { x: pTexte.x - embouchureT, y: pTexte.ym };
+	var viseeB = { x: pBloc.x + embouchureB + courbeB, y: pBloc.ym };
+	var viseeT = { x: pTexte.x - embouchureB - courbeB, y: pTexte.ym };
+	var ptM = { x: .5 * (viseeB.x + viseeT.x), y: .5 * (viseeB.y + viseeT.y) };
+	var lt = { a: (viseeB.y - viseeT.y) / (viseeT.x - viseeB.x) };
+	lt.b = lt.a * viseeB.x + viseeB.y;
+	// On cherche le point situé à n pixels à gauche du centre du tube (en allant du texte vers le bloc).
+	var racinea2plus1 = Math.sqrt(lt.a * lt.a + 1);
+	var dx = demieLargeurTube * lt.a / racinea2plus1;
+	var dy = demieLargeurTube / racinea2plus1;
+	var ptMbas = { x: ptM.x + dx, y: ptM.y + dy }; // p: point; t: tube; M: milieu; bas: bah…
+	var ptMhaut = { x: ptM.x - dx, y: ptM.y - dy };
+	// On cherche le 'b' des droites du haut et du bas du tube (le 'a' est identique, puisque les droites sont parallèles).
+	var lthaut = { b: lt.a * ptMhaut.x + ptMhaut.y };
+	var ltbas = { b: lt.a * ptMbas.x + ptMbas.y };
+	// Et l'intersection de ces mêmes droites avec le bout de tuyau horizontal partant qui du texte, qui du carreau.
+	var pcTbas = { x: (ltbas.b - (ptT.y + demieLargeurTube)) / lt.a, y: ptT.y + demieLargeurTube }; // c: coude.
+	var pcThaut = { x: (lthaut.b - (ptT.y - demieLargeurTube)) / lt.a, y: ptT.y - demieLargeurTube };
+	var pcBbas = { x: (ltbas.b - (ptB.y + demieLargeurTube)) / lt.a, y: ptB.y + demieLargeurTube }; // c: coude.
+	var pcBhaut = { x: (lthaut.b - (ptB.y - demieLargeurTube)) / lt.a, y: ptB.y - demieLargeurTube };
+	// Nos points de contrôle vont se trouver à l'intersection 
+	d += ' M '+pTexte.x+','+pTexte.y1+' C '+(pTexte.x - embouchureT)+','+pTexte.y1+' '+pTexte.x+','+pcTbas.y+' '+(pTexte.x - embouchureT)+','+pcTbas.y; // Accolade texte bas.
+	d += ' C '+(pcTbas.x - embouchureT)+','+pcTbas.y+' '+(pcTbas.x - embouchureT)+','+pcTbas.y+' '+ptMbas.x+','+ptMbas.y; // Coude, et remontée jusqu'au pivot.
+	d += ' C '+(pcBbas.x + embouchureB)+','+pcBbas.y+' '+(pcBbas.x + embouchureB)+','+pcBbas.y+' '+(pBloc.x + embouchureB)+','+pcBbas.y;
+	d += ' C '+pBloc.x+','+pcBbas.y+' '+(pBloc.x + embouchureB)+','+pBloc.y1+' '+pBloc.x+' '+pBloc.y1;
+	d += ' L '+pBloc.x+','+pBloc.y0+' C '+(pBloc.x + embouchureB)+','+pBloc.y0+' '+pBloc.x+','+pcBhaut.y+' '+(pBloc.x + embouchureB)+','+pcBhaut.y; // Longement bloc + accolade bloc haute.
+	d += ' C '+(pcBhaut.x + embouchureB)+','+pcBhaut.y+' '+(pcBhaut.x + embouchureB)+','+pcBhaut.y+' '+ptMhaut.x+','+ptMhaut.y;
+	d += ' C '+(pcThaut.x - embouchureT)+','+pcThaut.y+' '+(pcThaut.x - embouchureT)+','+pcThaut.y+' '+(pTexte.x - embouchureT)+','+pcThaut.y;
+	d += ' C '+pTexte.x+','+pcThaut.y+' '+(pTexte.x - embouchureT)+','+pTexte.y0+' '+pTexte.x+','+pTexte.y0;
+	d += ' z';
+	return d;
+};
+
 LignesTemps.calculer = function()
 {
 	var svg = document.getElementById('jonctionlignestemps');
