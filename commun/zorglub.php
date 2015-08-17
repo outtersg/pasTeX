b079f8cce9209f0cd7c427ce756bd1ed3f24d9c5
Guillaume Outters <guillaume-pasTeX@outters.eu>
2015-08-17 22:29:12 +0200

    zorglub::année()
    + Période réduite à sa plus simple expression (année).
    
    darcs-hash:d0efabcc1bfb2fb994543a968d25ee5e4d121d7f

--- commun/zorglub.php
+++ commun/zorglub.php
@@ -14,6 +14,19 @@ class Zorglub
 		if($d > 0)
 			return pasTeX_affDurée($d);
 	}
+	
+	public function année($projet)
+	{
+		$périodes = array();
+		foreach($projet->date as $période)
+			$périodes[] = array($période->d, $période->f);
+		$période = periode_union($périodes);
+		if($période[1][0] == -1)
+			$période[1] = Date::obtenir(time());
+		$période[0] = array($période[0][0], -1, -1, -1, -1, -1);
+		$période[1] = array($période[1][0], -1, -1, -1, -1, -1);
+		return Periode::aff(Date::mef($période[0]), Date::mef($période[1]));
+	}
 }
 
 ?>
