cd6fa9ac168659ca9e6993de6453e8fb44346018
Guillaume Outters <guillaume-pasTeX@outters.eu>
2015-10-08 07:03:08 +0200

    dateur: traduction en
    + Le Dateur sait (sur sa partie) traduire en anglais.
    + Implémentation du "depuis" / "depuis le" en français (selon que l'on parle d'une année ou d'un jour).
    # Le jusqu'à n'était pas implémenté. Enfin il manquait la partie date.
    
    darcs-hash:139f5a620326eae6cc7d53217cc608bcdf64c01c

--- commun/dateur.php
+++ commun/dateur.php
@@ -6,6 +6,8 @@ require_once dirname(__FILE__).'/../util/periode.inc';
 
 class Dateur extends \DateurMoinsUn
 {
+	public $langue;
+	
 	public function affPériode($d, $f, $mode = 0)
 	{
 		$null = array(-1, -1, -1, -1, -1, -1);
@@ -15,11 +17,36 @@ class Dateur extends \DateurMoinsUn
 			if($f === null)
 				return null;
 			else
-				return 'jusqu\'à'; /* À FAIRE: jusqu'au éventuellement */
+				return $this->_demiDroite($f, true, $mode);
 		if($f === null)
-			return 'depuis '.parent::aff($d, $mode);
+			return $this->_demiDroite($d, false, $mode);
 		return parent::affPériode($d, $f, $mode);
 	}
+	
+	/**
+	 * Sort la chaîne décrivant une période fermée à un bout mais pas l'autre.
+	 */
+	protected function _demiDroite($d, $jusquÀSinonDepuis = false, $mode)
+	{
+		$affDate = parent::aff($d, $mode);
+		$langue = isset($this->langue) && isset(self::$CHAÎNES[$this->langue]) ? $this->langue : 'fr';
+		$chaînes = self::$CHAÎNES[$langue];
+		
+		$pos = $jusquÀSinonDepuis ? 1 : 0;
+		// Mois et an sont des plages, mais le jour est précis: en français, on passe de "depuis" à "depuis le".
+		if($d[2] > 0)
+			$pos += 2;
+		if(!isset($chaînes[$pos]))
+			$pos %= 2;
+		
+		return strtr($chaînes[$pos], array('%d' => $affDate));
+	}
+	
+	protected static $CHAÎNES = array
+	(
+		'fr' => array('depuis %d', 'jusqu\'à %d', 'depuis le %d', 'jusqu\'au %d'),
+		'en' => array('since %d', 'until %d'),
+	);
 }
 
 ?>
